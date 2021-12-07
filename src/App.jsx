8d12f738f2201b8f4dbb825e0403ba8f23dc5a17
‘Katie <ktschr11@gmail.com>
2021-12-07 14:17:36 -0800

    rendered list of teams to the teamList view

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route, NavLink } from 'react-router-do
 import Home from './views/Home/Home'
 import './App.css'
 import TeamDetail from './views/Team/TeamDetail.jsx'
+import PlayerDetail from './views/Player/PlayerDetail.jsx'
 import TeamList from './views/Team/TeamList.jsx'
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
           </NavLink>
         </header>
         <Switch>
+          <Route path="/players/:id" exact component={PlayerDetail} />
           <Route path="/teams/:id" exact component={TeamDetail} />
           <Route path="/teams" exact component={TeamList} />
           <Route path="/" exact component={Home} />
--- src/views/Team/TeamList.jsx
+++ src/views/Team/TeamList.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getTeams } from '../../services/teams.js';
 import TeamDetail from './TeamDetail'
 
 function TeamList() {
+
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    getTeams().then((resp) => setTeams(resp));
+  }, []);
+
   return (
     <div>
-      Team View 
-      <TeamDetail/>
+      <h4>current baby teams</h4>
+      <ul>
+        {teams.map((team) => {
+          return (
+            <li key={team.id}>
+              <Link to={`teams/${team.id}`} className='App-link'>{team.name}</Link>
+            </li>
+          )
+        })
+
+        }
+      </ul>
     </div>
   )
 }
