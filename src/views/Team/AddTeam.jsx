474231813eefd8d3ec21c450e21d2656b2f37417
‘Katie <ktschr11@gmail.com>
2021-12-09 15:22:28 -0800

    add team form works!

--- src/views/Team/AddTeam.jsx
+++ src/views/Team/AddTeam.jsx
@@ -1,14 +1,21 @@
 import { useState } from 'react';
+import { useHistory} from 'react-router-dom';
+import { createTeam } from "../../services/teams.js"
 
 export default function AddTeam() {
+
   const [name, setName] = useState('');
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
 
+  const history = useHistory();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    alert('Handle Submit is werkin helllll yeah brotherrr')
+    const res = await createTeam({ name, city, state });
+
+    history.push(`/teams/${res[0].id}`);
   }
 
   return (
--- src/views/Team/TeamDetail.jsx
+++ src/views/Team/TeamDetail.jsx
@@ -10,15 +10,23 @@ export default function TeamDetail({ label, match }) {
   useEffect(() => {
     getTeamById(teamId)
     .then((resp) => {
+      console.log(resp)
       setTeam(resp)}).finally(() => setLoading(false));
   }, [teamId]);
 
-  if (loading) return <h1>looking for toddler teams</h1>;
+  if (loading) return (
+                  <>
+                    <h1>looking for toddler teams</h1>
+
+                  </>
+                  );
 
   if (team.players.length === 0) 
       return (
         <div>
+          <h1>{label}</h1>
           <h4>{team.name}</h4>
+          <h3>from {team.city}, {team.state}</h3>
           <p>still waiting on players</p>
         </div>
       );
@@ -27,6 +35,7 @@ export default function TeamDetail({ label, match }) {
     <div className='TeamDetail'>
       <h1>{label}</h1>
       <h4>{team.name}</h4>
+      <h3>from {team.city}, {team.state}</h3>
       {team.players.map((player) => {
         return (
           <p key={player.id}>
