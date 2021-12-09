a89c416bfc7a1702674df3c1240120191548e490
‘Katie <ktschr11@gmail.com>
2021-12-09 14:36:59 -0800

    form added;

--- src/views/Team/AddTeam.jsx
+++ src/views/Team/AddTeam.jsx
@@ -4,8 +4,16 @@ export default function AddTeam() {
       <fieldset>
         <legend>Add a Team</legend>
         <form action="">
-          <label htmlFor="name">team-name:</label>
+          <label htmlFor="name">team-name: </label>
           <input id='name' name='name' type="text" />
+
+          <label htmlFor="city">city: </label>
+          <input id='city' name='city' type="text" />
+
+          <label htmlFor="state">state: </label>
+          <input id='state' name='state' type="text" />
+
+          <input type="submit" value='add team' />
         </form>
       </fieldset>
     </>
