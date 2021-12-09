7f01ff257a97c5bf31956ba373fe8ec51bbdedbb
‘Katie <ktschr11@gmail.com>
2021-12-09 14:59:10 -0800

    handleSubmit triggering alert sucessfully

--- src/views/Team/AddTeam.jsx
+++ src/views/Team/AddTeam.jsx
@@ -5,31 +5,37 @@ export default function AddTeam() {
   const [city, setCity] = useState('');
   const [state, setState] = useState('');
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    alert('Handle Submit is werkin helllll yeah brotherrr')
+  }
+
   return (
     <>
       <fieldset>
         <legend>Add a Team</legend>
-        <form action="">
+        <form onSubmit={handleSubmit}>
           <label htmlFor="name">team-name: </label>
           <input
             id='name'
-            name='name'
+            name={name}
             type="text"
-            onChange={(e) => setName(e.target.value)} />
+            onChange={({ target }) => setName(target.value)} />
 
           <label htmlFor="city">city: </label>
           <input
             id='city'
-            name='city'
+            name={city}
             type="text"
-            onChange={(e) => setCity(e.target.value)} />
+            onChange={({ target }) => setCity(target.value)} />
 
           <label htmlFor="state">state: </label>
           <input
             id='state'
-            name='state'
+            name={state}
             type="text"
-            onChange={(e) => setState(e.target.value)} />
+            onChange={({ target }) => setState(target.value)} />
 
           <input type="submit" value='add team' />
         </form>
